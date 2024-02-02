309bc721395356ca04791e5f795cf27864fbb78e
Annie-Golightly <146282648+Annie-Golightly@users.noreply.github.com>
2024-02-02 16:00:53 +0000

    Task

--- tasks/task.c
+++ tasks/task.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+/*(
+    argv[1] is the first number
+    argv[2] is the second number
+)*/
 
-int main() {
+int main(int argc, char *argv[]) {
+    if (argc != 3){ //checking the correct number of arguments are entered
+        printf("Useage: ./task <num1><num2>\n");
+        return 0;
+    }
+
     int num1, num2;
-
-    // Prompt the user for input
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
-
-    printf("Enter the second number: ");
-    scanf("%d", &num2);
-
+    num1 = atoi(argv[1]); //storing the user inputs in relevant integer variables
+    num2 = atoi(argv[2]);
+    
     // Calculate and print the sum
     int sum = num1 + num2;
     printf("Sum: %d\n", sum);
