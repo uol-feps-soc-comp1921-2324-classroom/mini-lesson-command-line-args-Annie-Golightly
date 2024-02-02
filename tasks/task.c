fa19e0fa795b97b2ab4fefb235abc55bb9703823
Annie-Golightly <146282648+Annie-Golightly@users.noreply.github.com>
2024-02-02 16:48:21 +0000

    Trying out strtol

--- tasks/task.c
+++ tasks/task.c
@@ -10,14 +10,21 @@ int main(int argc, char *argv[]) {
         printf("Useage: ./task <num1><num2>\n");
         return 0;
     }
+    //int check;
+    char *p; //this points to where a char is in the provided string
+    long check = strtol(*argv,&p,10); //converts a string to long, stopping at characters
+    if( p == *argv )
+    {
+        printf("Not a number");
+    }
+    else{
+        int num1, num2;
+        num1 = atoi(argv[1]); //storing the user inputs in relevant integer variables
+        num2 = atoi(argv[2]);
+        
+        // Calculate and print the sum
+        int sum = num1 + num2;
+        printf("Sum: %d\n", sum);
 
-    int num1, num2;
-    num1 = atoi(argv[1]); //storing the user inputs in relevant integer variables
-    num2 = atoi(argv[2]);
-    
-    // Calculate and print the sum
-    int sum = num1 + num2;
-    printf("Sum: %d\n", sum);
-
-    return 0; // Exit successfully
+        return 0; }// Exit successfully
 }
